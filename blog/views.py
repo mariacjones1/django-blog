327ce561b9c8f8ea2085105d82bf184915059012
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-07-26 15:34:21 +0000

    Set up login and signup pages and authorisation

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ class PostDetail(View):
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-        
+
         return render(
             request,
             "post_detail.html",
